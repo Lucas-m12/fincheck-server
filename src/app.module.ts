c1f5e28f3d91ddf8d7998c08a5fab770563b1744
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2023-06-26 21:22:14 -0300

    feat: create category module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CategoryModule } from 'modules/category/category.module';
+import { UserModule } from 'modules/user/user.module';
+import { DbModule } from './shared/db/db.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, CategoryModule, DbModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
